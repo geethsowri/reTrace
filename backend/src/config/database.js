f2894661d034f48773d3117e38f5405bcbf0b42f
geethsowri <nainalageethsowri@gmail.com>
2025-06-19 16:59:06 +0530

    removed unwanted files

--- backend/src/config/database.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const connectDB = async() => {
-    await mongoose.connect(process.env.MONGO_URI);
-};
-
-mondule.exports = connectDB;
--- backend/src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authMiddleware = require("../middleware/authMiddleware");
-const userController = require("../controllers/userController");
-
-router.get("/me", authMiddleware, userController.viewProfile);
-router.put("/me", authMiddleware, userController.updateProfile);
-
-module.exports = router;
