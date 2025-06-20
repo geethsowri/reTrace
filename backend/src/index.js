8aa218fb687d8516edbb5492575e565e5397ef52
geethsowri <nainalageethsowri@gmail.com>
2025-06-20 07:24:00 +0530

    Added Vercel frontend URL

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,11 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(cors({
+  origin: 'https://retrace-eight.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true,
+}));
 
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
